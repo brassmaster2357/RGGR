f87214f077a087065a75a1bc7cc2bef491619e5f
Gopherdays <g4r64g3m4n@gmail.com>
2024-02-01 09:27:09 -0800

    Update zommie

--- Assets/Scripts/ZombieAI.cs
+++ Assets/Scripts/ZombieAI.cs
@@ -7,14 +7,26 @@ public class ZombieAI : MonoBehaviour
     GameObject target;
     Rigidbody2D myRB;
     public float speed;
+    int health;
 
     private void Start()
     {
-        target = GameObject.Find("Target");
+        target = GameObject.Find("Player");
         myRB = GetComponent<Rigidbody2D>();
+        health = 3;
     }
     private void Update()
     {
         myRB.AddForce((target.transform.position - transform.position).normalized * speed);
+        if (health <= 0)
+            Destroy(this.gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Projectile")
+        {
+            health -= 1;
+            Destroy(collision.gameObject);
+        }
     }
 }
