1ef60e2cb472c97cc4902dd347038f6b2624f786
obamagaming <91400278+brassmaster2357@users.noreply.github.com>
2024-02-02 10:12:44 -0800

    that's what I was thinking

--- Assets/Scripts/HUDControl.cs
+++ Assets/Scripts/HUDControl.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class HUDControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image healthBar;
+    public Image healthBarEmpty;
+    public TextMeshProUGUI moneyText;
+    public PlayerController pc;
+
     void Start()
     {
-        
+        pc = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
     }
