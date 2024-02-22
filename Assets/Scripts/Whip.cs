f3769c3b978660d7fdbbf5185d9e2adfc00b289f
Tyler Bowers <strikrrt@outlook.com>
2024-02-21 20:19:50 -0800

    whip

--- Assets/Scripts/Whip.cs
+++ Assets/Scripts/Whip.cs
@@ -1,8 +1,39 @@
 using UnityEngine;
 
+public enum WhipDirection
+{
+    Up,
+    Down,
+    Left,
+    Right    
+}
+
 public class Whip : MonoBehaviour
 {
-   private void OnCollisionEnter2D(Collision2D collision)
+    public WhipDirection whipDirection;
+    public Transform Player;
+
+    private void Update()
+    {
+        if (whipDirection == WhipDirection.Up)
+        {
+            transform.position = new Vector3(Player.position.x, Player.position.y + 1.1f, 0);
+        }
+        else if (whipDirection == WhipDirection.Down)
+        {
+            transform.position = new Vector3(Player.position.x, Player.position.y - 1.1f, 0);
+        }
+        else if (whipDirection == WhipDirection.Left)
+        {
+            transform.position = new Vector3(Player.position.x - 1.2f, Player.position.y - 0.4f, 0);
+        }
+        else if (whipDirection == WhipDirection.Right)
+        {
+            transform.position = new Vector3(Player.position.x + 1.2f, Player.position.y - 0.4f, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision.gameObject.name);
         //if collide with enemy, deal damage
